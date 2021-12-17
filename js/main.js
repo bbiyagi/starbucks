71ab4ce5890a477210d9a3d1ee597ea17c5bc842
bbiyagi <bulehychoi@naver.com>
2021-12-17 16:16:27 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function () {
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused');
-    // 속성이름, 속성
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused');
-    // 속성이름, 속성
-    searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 // lodash 연결 300주는이유는 throttle: 스크롤이벤트시 횟수부하를 줄이기 위해서 
@@ -167,7 +147,3 @@ spyEls.forEach(function (spyEl) {
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller());
 });
-
-// copyright this-year
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021 
